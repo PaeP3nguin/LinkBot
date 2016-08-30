eb089787bb9bc1ace958473ffc2379318447e50a
William <paep3nguin@gmail.com>
2016-08-29 22:44:36 -0700

    LinkBot listens to options

--- options/options.js
+++ options/options.js
@@ -1,6 +1,9 @@
 var form = $('#form');
+
 var linkOnLoad = $('#link_on_load');
 var linkOnChange = $('#link_on_change');
+var linkEmails = $('#link_emails');
+
 var statusText = $('#status');
 var reset = $('#reset');
 
@@ -9,7 +12,8 @@ function saveOptions() {
   statusText.text('Options saved!');
   chrome.storage.sync.set({
     linkOnLoad: linkOnLoad.prop('checked'),
-    linkOnChange: linkOnChange.prop('checked')
+    linkOnChange: linkOnChange.prop('checked'),
+    linkEmails: linkEmails.prop('checked')
   }, function() {
     statusText.fadeIn();
   });
@@ -18,10 +22,12 @@ function saveOptions() {
 function getOptions() {
   chrome.storage.sync.get({
     linkOnLoad: true,
-    linkOnChange: true
-  }, function(items) {
-    linkOnLoad.prop('checked', items.linkOnLoad);
-    linkOnChange.prop('checked', items.linkOnChange);
+    linkOnChange: true,
+    linkEmails: true
+  }, function(options) {
+    linkOnLoad.prop('checked', options.linkOnLoad);
+    linkOnChange.prop('checked', options.linkOnChange);
+    linkEmails.prop('checked', options.linkOnChange);
   });
 }
 
@@ -30,10 +36,12 @@ function resetDefaults() {
   statusText.text('Options reset');
   chrome.storage.sync.set({
     linkOnLoad: true,
-    linkOnChange: true
+    linkOnChange: true,
+    linkEmails: true
   }, function() {
     linkOnLoad.prop('checked', true);
     linkOnChange.prop('checked', true);
+    linkEmails.prop('checked', options.linkEmails);
     statusText.fadeIn();
   });
 }
--- url_to_link.js
+++ url_to_link.js
@@ -1,29 +1,3 @@
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-var observer = new MutationObserver(function(mutations) {
-  console.log('mutation!');
-  observer.disconnect();
-  mutations.forEach(function(m) {
-    if (m.type === "characterData") {
-      // console.log(m);
-    } else if (m.type === "childList") {
-      // console.log(m);
-      m.addedNodes.forEach(function(node) {
-        if (node.nodeType == Node.TEXT_NODE) {
-          linkifyNode(node);
-        } else {
-          convertLinks(node);
-        }
-      });
-    }
-  });
-  observer.observe(document.body, {
-    subtree: true,
-    characterData: true,
-    childList: true
-  });
-});
-
 // Temporary placeholder for potentially conflicting email substitution
 var TEMP_CHAR = '\uFFFF';
 var TEMP_CHAR_REGEX = /\uFFFF/gi;
@@ -55,15 +29,51 @@ var EXCLUDED_TAGS = {
   CODE: true,
 };
 
-function convertLinks(root) {
+// Get options, then run convert links as needed
+chrome.storage.sync.get({
+  linkOnLoad: true,
+  linkOnChange: true,
+  linkEmails: true
+}, function(options) {
+  if (options.linkOnLoad) {
+    convertLinks(document.body, options);
+  }
+
+  if (options.linkOnChange) {
+    var observeOptions = {
+      subtree: true,
+      characterData: true,
+      childList: true
+    };
+
+    var observer = new MutationObserver(function(mutations) {
+      observer.disconnect();
+      mutations.forEach(function(m) {
+        if (m.type === "characterData") {
+          // console.log(m);
+        } else if (m.type === "childList") {
+          m.addedNodes.forEach(function(node) {
+            if (node.nodeType == Node.TEXT_NODE) {
+              linkifyNode(node, options);
+            } else {
+              convertLinks(node, options);
+            }
+          });
+        }
+      });
+      observer.observe(document.body, observeOptions);
+    });
+
+    observer.observe(document.body, observeOptions);
+  }
+});
+
+// Function to convert all links
+function convertLinks(root, options) {
   // Initialize a TreeWalker to start looking at text from the root node
-  var walker = document.createTreeWalker(
-    root,
-    NodeFilter.SHOW_ALL, {
-      acceptNode: excludeNodes
-    },
-    false
-  );
+  var walker = document.createTreeWalker(root, NodeFilter.SHOW_ALL, {
+    acceptNode: excludeNodes
+  }, false);
 
   var node = walker.nextNode();
 
@@ -72,47 +82,7 @@ function convertLinks(root) {
   }
 
   while (node !== null) {
-    linkifyNode(node, nextNode, nextNode);
-  }
-}
-
-function linkifyNode(node, onBeforeReplace, onSkipped) {
-  // Email saving variables and functions
-  var emails = [];
-  var i = 0;
-
-  function stashEmail(email) {
-    emails.push('<a href="mailto:' + email + '">' + email + '</a>');
-    return TEMP_CHAR;
-  }
-
-  function getEmail() {
-    return emails[i++];
-  }
-
-  // Save the text to compare with later
-  var oldText = node.data;
-
-  // Save emails and replace with a temporary, noncharacter Unicode character
-  // We'll put the emails back in later
-  // Why? Because otherwise the part after the @ sign will be recognized and replaced as a URL!
-  var newText = oldText.replace(EMAIL_REGEX, stashEmail);
-
-  // Replace URLs with links
-  newText = newText.replace(URL_REGEX, '<a href="//$1">$&</a>');
-
-  // Put emails back in
-  newText = newText.replace(TEMP_CHAR_REGEX, getEmail);
-
-  if (newText !== oldText) {
-    // If we successfully added any links
-    console.log(newText);
-    if (onBeforeReplace !== undefined) {
-      onBeforeReplace();
-    }
-    $(node).replaceWith(newText);
-  } else if (onSkipped !== undefined) {
-    onSkipped();
+    linkifyNode(node, options, nextNode, nextNode);
   }
 }
 
@@ -137,12 +107,47 @@ function excludeNodes(node) {
   return NodeFilter.FILTER_ACCEPT;
 }
 
-setTimeout(function() {
-  convertLinks(document.body);
+function linkifyNode(node, options, onBeforeReplace, onSkipped) {
+  // Email saving variables and functions
+  var emails = [];
+  var i = 0;
 
-  observer.observe(document.body, {
-    characterData: true,
-    childList: true,
-    subtree: true
-  });
-}, 1500);
+  function stashEmail(email) {
+    emails.push('<a href="mailto:' + email + '">' + email + '</a>');
+    return TEMP_CHAR;
+  }
+
+  function getEmail() {
+    return emails[i++];
+  }
+
+  // Save the text to compare with later
+  var oldText = node.data;
+  var newText = oldText;
+
+  if (options.linkEmails) {
+    // Save emails and replace with a temporary, noncharacter Unicode character
+    // We'll put the emails back in later
+    // Why? Because otherwise the part after the @ sign will be recognized and replaced as a URL!
+    newText = newText.replace(EMAIL_REGEX, stashEmail);
+  }
+
+  // Replace URLs with links
+  newText = newText.replace(URL_REGEX, '<a href="//$1">$&</a>');
+
+  if (options.linkEmails) {
+    // Put emails back in
+    newText = newText.replace(TEMP_CHAR_REGEX, getEmail);
+  }
+
+  if (newText !== oldText) {
+    // If we successfully added any links
+    console.log(newText);
+    if (onBeforeReplace !== undefined) {
+      onBeforeReplace();
+    }
+    $(node).replaceWith(newText);
+  } else if (onSkipped !== undefined) {
+    onSkipped();
+  }
+}
