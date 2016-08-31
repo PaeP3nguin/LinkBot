a23916df79908a541e12ea4f08490fc477c23b05
William <paep3nguin@gmail.com>
2016-08-30 23:38:10 -0700

    Slight update to options JS to align with browser actions

--- options/options.js
+++ options/options.js
@@ -1,52 +1,54 @@
-var form = $('#form');
+$(function() {
+  var form = $('#form');
 
-var linkOnLoad = $('#link_on_load');
-var linkOnChange = $('#link_on_change');
-var linkEmails = $('#link_emails');
+  var linkOnLoad = $('#link_on_load');
+  var linkOnChange = $('#link_on_change');
+  var linkEmails = $('#link_emails');
 
-var statusText = $('#status');
-var reset = $('#reset');
+  var statusText = $('#status');
+  var reset = $('#reset');
 
-function saveOptions() {
-  statusText.hide();
-  statusText.text('Options saved!');
-  chrome.storage.sync.set({
-    linkOnLoad: linkOnLoad.prop('checked'),
-    linkOnChange: linkOnChange.prop('checked'),
-    linkEmails: linkEmails.prop('checked')
-  }, function() {
-    statusText.fadeIn();
-  });
-}
+  getOptions();
 
-function getOptions() {
-  chrome.storage.sync.get({
-    linkOnLoad: true,
-    linkOnChange: true,
-    linkEmails: true
-  }, function(options) {
-    linkOnLoad.prop('checked', options.linkOnLoad);
-    linkOnChange.prop('checked', options.linkOnChange);
-    linkEmails.prop('checked', options.linkOnChange);
-  });
-}
+  $(':input').change(saveOptions);
+  reset.click(resetDefaults);
 
-function resetDefaults() {
-  statusText.hide();
-  statusText.text('Options reset');
-  chrome.storage.sync.set({
-    linkOnLoad: true,
-    linkOnChange: true,
-    linkEmails: true
-  }, function() {
-    linkOnLoad.prop('checked', true);
-    linkOnChange.prop('checked', true);
-    linkEmails.prop('checked', options.linkEmails);
-    statusText.fadeIn();
-  });
-}
+  function saveOptions() {
+    statusText.hide();
+    statusText.text('Options saved!');
+    chrome.storage.sync.set({
+      linkOnLoad: linkOnLoad.prop('checked'),
+      linkOnChange: linkOnChange.prop('checked'),
+      linkEmails: linkEmails.prop('checked')
+    }, function() {
+      statusText.fadeIn();
+    });
+  }
 
-getOptions();
+  function getOptions() {
+    chrome.storage.sync.get({
+      linkOnLoad: true,
+      linkOnChange: true,
+      linkEmails: true
+    }, function(options) {
+      linkOnLoad.prop('checked', options.linkOnLoad);
+      linkOnChange.prop('checked', options.linkOnChange);
+      linkEmails.prop('checked', options.linkOnChange);
+    });
+  }
 
-$(':input').change(saveOptions);
-reset.click(resetDefaults);
+  function resetDefaults() {
+    statusText.hide();
+    statusText.text('Options reset');
+    chrome.storage.sync.set({
+      linkOnLoad: true,
+      linkOnChange: true,
+      linkEmails: true
+    }, function() {
+      linkOnLoad.prop('checked', true);
+      linkOnChange.prop('checked', true);
+      linkEmails.prop('checked', options.linkEmails);
+      statusText.fadeIn();
+    });
+  }
+});
