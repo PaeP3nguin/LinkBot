61fb0c229ae2b999a643a43c1e152db5c93a2bd4
William <paep3nguin@gmail.com>
2016-08-31 23:58:45 -0700

    Fix indentation in browser_action.js

--- browser_action/browser_action.js
+++ browser_action/browser_action.js
@@ -1,65 +1,65 @@
 $(function() {
-    var statusText = $('#status-text');
-    var reLink = $('#re-link');
-    var options = $('#options');
-    var help = $('#help');
+  var statusText = $('#status-text');
+  var reLink = $('#re-link');
+  var options = $('#options');
+  var help = $('#help');
 
-    reLink.click(linkCurrentPage);
-    options.click(openOptions);
-    help.click(openHelp);
+  reLink.click(linkCurrentPage);
+  options.click(openOptions);
+  help.click(openHelp);
 
-    // Send a message to the content script telling it to find links in the page again
-    function linkCurrentPage() {
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {
-                link: 'all'
-            }, function(response) {
-                if (response) {
-                    // Looked for links successfully
-                    updateStatus(response.linksFound);
-                } else {
-                    // Didn't get a response, might be on chrome page or page loading
-                    // Show no links found
-                    updateStatus(0);
-                }
-            });
-        });
-    }
-
-    // Timer handle for the fade-in of original button text
-    var statusFade;
-
-    function updateStatus(linksFound) {
-        var linkText = ' ' + (linksFound ? linksFound : 'No') + ' link' + (linksFound === 1 ? '' : 's') + ' found';
-        statusText.fadeOut(200, function() {
-            statusText.text(linkText);
-            statusText.fadeIn();
-        });
-
-        clearTimeout(statusFade);
-        statusFade = setTimeout(function() {
-            statusText.fadeOut(400, function() {
-                statusText.text('Find links');
-                statusText.fadeIn();
-            });
-        }, 2000);
-    }
-
-    function openOptions() {
-        if (chrome.runtime.openOptionsPage) {
-            // New way to open options pages, if supported (Chrome 42+).
-            chrome.runtime.openOptionsPage();
+  // Send a message to the content script telling it to find links in the page again
+  function linkCurrentPage() {
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        link: 'all'
+      }, function(response) {
+        if (response) {
+          // Looked for links successfully
+          updateStatus(response.linksFound);
         } else {
-            // Reasonable fallback.
-            window.open(chrome.runtime.getURL('options.html'));
+          // Didn't get a response, might be on chrome page or page loading
+          // Show no links found
+          updateStatus(0);
         }
-    }
+      });
+    });
+  }
 
-    function openHelp() {
-        // TODO: replace with actual ID when extension published
-        window.open('https://chrome.google.com/webstore/detail/' +  chrome.runtime.id + '/support');
+  // Timer handle for the fade-in of original button text
+  var statusFade;
+
+  function updateStatus(linksFound) {
+    var linkText = ' ' + (linksFound ? linksFound : 'No') + ' link' + (linksFound === 1 ? '' : 's') + ' found';
+    statusText.fadeOut(200, function() {
+      statusText.text(linkText);
+      statusText.fadeIn();
+    });
+
+    clearTimeout(statusFade);
+    statusFade = setTimeout(function() {
+      statusText.fadeOut(400, function() {
+        statusText.text('Find links');
+        statusText.fadeIn();
+      });
+    }, 2000);
+  }
+
+  function openOptions() {
+    if (chrome.runtime.openOptionsPage) {
+      // New way to open options pages, if supported (Chrome 42+).
+      chrome.runtime.openOptionsPage();
+    } else {
+      // Reasonable fallback.
+      window.open(chrome.runtime.getURL('options.html'));
     }
+  }
+
+  function openHelp() {
+    // TODO: replace with actual ID when extension published
+    window.open('https://chrome.google.com/webstore/detail/' + chrome.runtime.id + '/support');
+  }
 });
