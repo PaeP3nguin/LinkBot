213e5a8d86cdd99ade739e122744ad914311e19a
paep3nguin <paep3nguin@gmail.com>
2016-09-21 11:03:50 -0500

    Fix loader exclude

--- content_scripts/loader.js
+++ content_scripts/loader.js
@@ -4,7 +4,7 @@ chrome.storage.sync.get(DEFAULT_OPTIONS, function(options) {
     OPTIONS = options;
 
     // Only run if the current domain isn't excluded!
-    if (!OPTIONS.excludedHostnames.hasOwnProperty(window.location.hostName)) {
+    if (!OPTIONS.excludedHostnames.hasOwnProperty(window.location.hostname)) {
         chrome.runtime.sendMessage({"inject": true});
     }
 });
