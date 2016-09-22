1bf265d21d6fc46e84f64bc1453f76719e0edffe
paep3nguin <paep3nguin@gmail.com>
2016-09-22 00:13:17 -0500

    Relocate default_options.js

---
+++ options/default_options.js
@@ -0,0 +1,14 @@
+var DEFAULT_OPTIONS = {
+    linkOnLoad: true,
+    linkOnChange: true,
+    linkEmails: true,
+    excludedHostnames: {}
+};
+
+var DEFAULT_EXCLUDED_HOSTNAMES = {
+    "www.facebook.com": true,
+    "twitter.com": true,
+    "www.tumblr.com": true,
+    "mail.google.com": true,
+    "www.google.com": true,
+};
